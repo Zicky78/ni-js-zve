80f0e84a1699297f7c060aeb1de0834509a30768
Zicky78 <16zns01@gmail.com>
2019-02-28 20:56:30 -0800

    Updated resources

--- Experimentation/data.js
+++ Experimentation/data.js
@@ -1,5 +1,6 @@
 const resources = [];
 
+//Geekwise
 const geekwiseResource = new resource(
     "https://geekwiseacademy.com/",
     "./geekwise.jpg",
@@ -15,16 +16,44 @@ const geekwiseResource = new resource(
 
 resources.push(geekwiseResource);
 
+//Fresno City College
+const fccResource = new resource(
+    "https://www.fresnocitycollege.edu",
+    "./fcc.png",
+    "Fresno City College",
+    "Fresno's best community college, offering many classes for career paths, and offering transfer opportunities to universities",
+    "1101 E University Ave, Fresno, CA 93741",
+    "(559) 442-4600",
+    false,
+    false,
+    true,
+    "School"
+)
+
+resources.push(fccResource);
+
+//Clovis Community College
+const cccResource = new resource(
+    "https://www.cloviscollege.edu",
+    "./ccc.jpg",
+    "Clovis Community College",
+    "Clovis' best community college, offering many classes for career paths, and offering transfer opportunities to universities",
+    "10309 N Willow Ave, Fresno, CA 93730",
+    "(559) 325-5200",
+    false,
+    false,
+    true,
+    "School"
+)
+
+resources.push(cccResource);
+
+for ( i=0; i<3; i++ ) {
+    console.log(resources[i]);
+}
 
 
-
-
-
-
-
-
-
-function resource(hlink, imgSrc, name, description, location, phoneNumber, free, affordable, expensive, career) {
+function resource(hlink, imgSrc, name, description, location, phoneNumber, free, affordable, expensive, tag) {
     this.hlink = hlink;
     this.imgSrc = imgSrc;
     this.name = name;
@@ -34,7 +63,7 @@ function resource(hlink, imgSrc, name, description, location, phoneNumber, free,
     this.free = free;
     this.affordable = affordable;
     this.expensive = expensive;
-    this.career = career;
+    this.tag = tag;
     return this;
 }
 
