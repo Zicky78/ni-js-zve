7327a1206bc86eac3611050c3cc09da23b8e830c
Zicky78 <16zns01@gmail.com>
2019-03-26 16:59:39 -0700

    updated carousel and html targeting

--- 559 Project/targetHTML.js
+++ 559 Project/targetHTML.js
@@ -1,11 +1,11 @@
 function target() {
-    for(i = 0; i < 3; i++) {
+    for(i = 0; i < 4; i++) {
         document.getElementById(`c1-name${i+1}`).innerHTML = finalResources[i].name;
         document.getElementById(`c1-desc${i+1}a`).innerHTML = finalResources[i].description;
         document.getElementById(`c1-loc${i+1}`).innerHTML = finalResources[i].location;
         document.getElementById(`c1-phone${i+1}`).innerHTML = finalResources[i].phoneNumber;
         document.getElementById(`c1-img${i+1}`).src = finalResources[i].imgSrc;
-        //document.getElementById(`c1-link${i+1}`).href = finalResources[i].hlink;
+        document.getElementById(`c1-link${i+1}`).href = finalResources[i].hlink;
     }
 }
 
